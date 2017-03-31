c223aba0f809837fe395e1dee396d69ff18f6770
Miguel Caligares <mcaligares@theinit.com>
2017-03-31 20:45:27 -0300

    added yargs dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var yargs = require('yargs');
+
+var enabledLanguages = [
+    'javascript'
+];
+
+var args = yargs
+        .alias('u', 'username').describe('u', 'your username').default('u', 'anonymous')
+        .alias('l', 'language').describe('l', 'your language').default('l', 'js')
+            .choices('l', enabledLanguages)
+        .argv;
 
 gulp.task('default', function() {
-  // place code for your default task here
+    console.log('username:', args.username);
+    console.log('language:', args.language);
 });
