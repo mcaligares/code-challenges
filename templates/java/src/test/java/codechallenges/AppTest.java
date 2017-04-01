c679fda0d0014e3b1def8e8a421332e388814b3d
Miguel Caligares <mcaligares@theinit.com>
2017-04-01 19:04:54 -0300

    added java template test

---
+++ templates/java/src/test/java/codechallenges/AppTest.java
@@ -0,0 +1,12 @@
+package codechallenges;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testSomething() {
+        System.out.println("your awesome test");
+        Assert.assertTrue(true);
+    }
+}
