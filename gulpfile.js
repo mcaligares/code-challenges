d323a74e5cc7cef45018853a42c56595b21215b2
Miguel Caligares <mcaligares@theinit.com>
2017-04-01 00:58:09 -0300

    added config file

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,20 @@ var yargs = require('yargs');
 var copydir = require('copy-dir');
 //TODO add readline option
 
-//TODO use a file config
-var enabledLanguages = [
-    'javascript'
-];
-
-var anonymous = 'anonymous';
-var usersPath = 'users';
-var templatesPath = 'templates';
-var challengesPath = 'challenges';
+var settings = JSON.parse(fs.readFileSync('./config/settings.json', 'utf8'));
 
 var args = yargs
-        .alias('u', 'username').describe('u', 'your username').default('u', 'anonymous')
-        .alias('l', 'language').describe('l', 'your language').default('l', 'js')
-            .choices('l', enabledLanguages)
+        .alias('u', 'username').describe('u', 'your username').default('u', settings.default.username)
+        .alias('l', 'language').describe('l', 'your language').default('l', settings.default.language)
+            .choices('l', settings.languages)
         .argv;
-
-//TODO use a file config
-var ignoreFiles = {
-    'javascript': [
-        'node_modules'
-    ]
-};
 
 gulp.task('default', ['start']);
 
 gulp.task('check', function() {
-    if (!fs.existsSync(usersPath)) fs.mkdirSync(usersPath);
-    if (!fs.existsSync(templatesPath)) fs.mkdirSync(templatesPath);
-    if (!fs.existsSync(challengesPath)) fs.mkdirSync(challengesPath);
+    if (!fs.existsSync(settings.usersPath)) fs.mkdirSync(settings.usersPath);
+    if (!fs.existsSync(settings.templatesPath)) fs.mkdirSync(settings.templatesPath);
+    if (!fs.existsSync(settings.challengesPath)) fs.mkdirSync(settings.challengesPath);
 });
 
 gulp.task('start', ['check'], function() {
@@ -40,11 +25,11 @@ gulp.task('start', ['check'], function() {
 
     var username = args.username;
     var language = args.language;
-    var userWorkspace = usersPath + '/' + username;
+    var userWorkspace = settings.usersPath + '/' + username;
 
     if (!fs.existsSync(userWorkspace)) fs.mkdirSync(userWorkspace);
 
-    var lastChallange = getLastFileModified(fs.readdirSync(challengesPath));
+    var lastChallange = getLastFileModified(fs.readdirSync(settings.challengesPath));
     if (lastChallange) {
         var userChallangePath = userWorkspace + '/' + lastChallange.name;
         buildWorkspaceToChallange(userChallangePath, language);
@@ -59,7 +44,7 @@ function getLastFileModified(/*Array*/ files) {
     var resultFile;
     for (var i in files) {
         var filename = files[i];
-        var file = fs.statSync(challengesPath + '/' + filename);
+        var file = fs.statSync(settings.challengesPath + '/' + filename);
         if (!resultFile) {
             resultFile = {name: filename, file: file};
         } else if (resultFile.file.mtime < file.mtime) {
@@ -70,15 +55,17 @@ function getLastFileModified(/*Array*/ files) {
 }
 
 function buildWorkspaceToChallange(userChallengeWorkspacePath, language) {
-    var templateForLanguagePath = templatesPath + '/' + language;
+    var templateForLanguagePath = settings.templatesPath + '/' + language;
     if (fs.existsSync(templateForLanguagePath)) {
         if (!fs.existsSync(userChallengeWorkspacePath)) {
+
+            console.log('creando workspace en ' + userChallengeWorkspacePath);
             fs.mkdirSync(userChallengeWorkspacePath);
             copydir.sync(templateForLanguagePath, userChallengeWorkspacePath);
             ignoreFilesOnWorkspace(userChallengeWorkspacePath, language);
             //TODO go to workspace and run test
         }
-        if (args.username != 'anonymous') {
+        if (args.username != settings.anonymous) {
             console.log('you are ready to work! good luck ' + args.username +'!');
         }
     } else {
@@ -88,14 +75,14 @@ function buildWorkspaceToChallange(userChallengeWorkspacePath, language) {
 
 function ignoreFilesOnWorkspace(path, language) {
     var gitIgnoreFile = path + '/.gitignore';
-    var ignoredFilesByLanguage = ignoreFiles[language];
+    var ignoredFilesByLanguage = settings.ignoreFiles[language];
     for (var i in ignoredFilesByLanguage) {
         fs.writeFileSync(gitIgnoreFile, ignoredFilesByLanguage[i]);
     }
 }
 
 function readChallenge(challenge) {
-    var challengeFile = challengesPath + '/' + challenge.name;
+    var challengeFile = settings.challengesPath + '/' + challenge.name;
     var text = fs.readFileSync(challengeFile, {encoding: 'utf-8'});
     console.log('*** ' + challenge.name + ' ***');
     console.log(text);
